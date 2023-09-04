1902c44338c195f9cccc8aa03d7ad81135575665
dasternd <danil@stepanov.name>
2023-09-04 21:31:45 +0300

    config site

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,7 +111,7 @@ let resumeButton = document.getElementById('resume-button');
 // Html2pdf options
 let opt = {
   margin: 1,
-  filename: 'myResume.pdf',
+  filename: 'CV-DanilStepanov.pdf',
   image: { type: 'jpeg', quality: 0.98 },
   html2canvas: { scale: 4 },
   jsPDF: { format: 'a4', orientation: 'portrait' },
